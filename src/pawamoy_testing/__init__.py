158b91b5c5752a6d635a6efbc59819960d649941
Timothée Mazzucotelli <dev@pawamoy.fr>
2026-02-12 13:28:52 +0000

    feat: Initial commit

---
+++ src/pawamoy_testing/__init__.py
@@ -0,0 +1,10 @@
+"""Pawamoy Testing package.
+
+Testing this great template
+"""
+
+from __future__ import annotations
+
+from pawamoy_testing._internal.cli import get_parser, main
+
+__all__: list[str] = ["get_parser", "main"]
